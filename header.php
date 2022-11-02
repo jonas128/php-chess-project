799f674692a390c7a3569db33fb68ec18f592516
jonas128 <jonasm@outlook.com>
2022-11-02 22:35:40 +0100

    update header.php delete rook.php

--- header.php
+++ header.php
@@ -5,10 +5,14 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CHESS</title>
+    <title>SCHACK</title>
 
-    <link rel="stylesheet" href="style/style.css" />
     <link rel="stylesheet" href="style/button.css" />
-
-
+    <link rel="stylesheet" href="style/style.css" />
+    <link rel="stylesheet" href="style/mediascreen.css" />
+    <link rel="stylesheet" href="style/article.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 </head>
+
+<body>
--- rook.php
+++
@@ -1,2 +0,0 @@
-<?php
-require __DIR__ . '/board.php';
